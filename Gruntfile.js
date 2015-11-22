c6d8859f4534ed11c45f6d9216c33ad6df3d4054
Kimmyrae <kimberly.mcbain@gmail.com>
2015-11-22 15:53:52 -0800

    added grunt build and build control

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,9 +16,9 @@ module.exports = function (grunt) {
   require('jit-grunt')(grunt, {
     useminPrepare: 'grunt-usemin',
     ngtemplates: 'grunt-angular-templates',
-    cdnify: 'grunt-google-cdn'
-  });
-
+    cdnify: 'grunt-google-cdn',
+    buildcontrol: 'grunt-build-control'
+});
   // Configurable paths for the application
   var appConfig = {
     app: require('./bower.json').appPath || 'app',
@@ -30,6 +30,20 @@ module.exports = function (grunt) {
 
     // Project settings
     yeoman: appConfig,
+    buildcontrol: {
+   options: {
+     dir: 'dist',
+     commit: true,
+     push: true,
+     message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+   },
+   pages: {
+     options: {
+       remote: 'git@github.com:your_github_user/your_webapp.git',
+       branch: 'gh-pages'
+     }
+   }
+ },
 
     // Watches files for changes and runs tasks based on the changed files
     watch: {
