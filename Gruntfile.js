b758d24d340f50fb12ea4f5dfc146671278f8deb
Kimmyrae <kimberly.mcbain@gmail.com>
2015-11-22 16:05:19 -0800

    added grunt buildcontrol

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,20 +30,22 @@ module.exports = function (grunt) {
 
     // Project settings
     yeoman: appConfig,
+
+
     buildcontrol: {
-   options: {
-     dir: 'dist',
-     commit: true,
-     push: true,
-     message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
-   },
-   pages: {
-     options: {
-       remote: 'git@github.com:your_github_user/your_webapp.git',
-       branch: 'gh-pages'
-     }
-   }
- },
+      options: {
+        dir: 'dist',
+        commit: true,
+        push: true,
+        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+      },
+      pages: {
+        options: {
+          remote: 'git@github.com:your_github_user/your_webapp.git',
+          branch: 'gh-pages'
+        }
+      }
+    },
 
     // Watches files for changes and runs tasks based on the changed files
     watch: {
