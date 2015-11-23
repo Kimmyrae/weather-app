4f0406946e600c5eb9621d873755446367bf2425
Kimmyrae <kimberly.mcbain@gmail.com>
2015-11-22 17:08:08 -0800

    added ssh keys to grunt file.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,7 +41,7 @@ module.exports = function (grunt) {
       },
       pages: {
         options: {
-          remote: 'git@github.com:your_github_user/your_webapp.git',
+          remote: 'git@github.com:Kimmyrae/weather-app.git',
           branch: 'gh-pages'
         }
       }
